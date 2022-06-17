7d052a9e7bf667d288399a06ea40e8ae02f4b244
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-06-17 19:34:41 +0200

    api/people: `checkimg server variable on deploy

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production';
 
 export const server = dev
-  ? 'http://localhost:3000/'
-  : 'https://next-api-template.vercel.app/';
+  ? 'http://localhost:3000'
+  : 'https://next-api-template.vercel.app';
