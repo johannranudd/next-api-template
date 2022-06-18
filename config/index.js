ed1258f108591ba7f0d3ed483969191aa94bb5b0
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-06-18 22:05:02 +0200

    config/index https

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== 'production';
 
 export const server = dev
   ? 'http://localhost:3000'
-  : 'http://next-api-template.vercel.app';
+  : 'https://next-api-template.vercel.app';
